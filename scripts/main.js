707363bee9460193f35a0837eb2f570a53bcd9b9
Alface <pedrocesar1410@gmail.com>
2019-03-21 20:18:40 -0300

    Create functions to go left and go right using arrows

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,14 @@ class Table {
         this.createTableElement(rows, columns);
     }
 
+    static getCellElement(i, j) {
+        return document.querySelectorAll('.' + Table.getColumnName(j))[i];
+    }
+
+    static getColumnName(j) {
+        return String.fromCharCode(j + 64);
+    }
+
     createTableElement() {
         const table = document.createElement('table');
 
@@ -58,26 +66,36 @@ class Table {
     createDefaultCellElement(i, j) {
         const that = this;
         const cell = document.createElement('input');
-        const columnName = String.fromCharCode(64 + j);
-        cell.classList.add(columnName);
+        cell.classList.add(Table.getColumnName(j));
         cell.addEventListener('change', function() {
             if(cell.value.charAt(0) == '=') {
-                cell.value = that.getFormulaResult(cell.value.substr(1, cell.value.length - 1));
+                cell.value = that.getFormulaResult(cell.value);
             }
         });
         cell.addEventListener('keydown', function(e) {
-            if(e.key == 'ArrowUp' && i > 1)
-                document.querySelectorAll('.' + columnName)[i - 1].focus();
-            else if(e.key == 'ArrowDown' && i < that.rows)
-                document.querySelectorAll('.' + columnName)[i + 1].focus();
+            switch(e.key) {
+                case 'ArrowUp':
+                    if(i > 1) Table.getCellElement(i - 1, j).focus();
+                    break;
+                case 'ArrowDown':
+                    if(i < that.rows) Table.getCellElement(i + 1, j).focus();
+                    break;
+                case 'ArrowLeft':
+                    if(j > 1 && cell.selectionStart == 0 && cell.selectionEnd == 0)
+                        Table.getCellElement(i, j - 1).focus();
+                    break;
+                case 'ArrowRight':
+                    if(j < that.columns && cell.selectionStart == cell.value.length && cell.selectionEnd == cell.value.length)
+                        Table.getCellElement(i, j + 1).focus();
+                    break;
+            }
         });
         return cell;
     }
 
     getFormulaResult(src) {
-        let result = "";
-        let readingCell = false;
-        for(let i = 0; i < src.length; i++) {
+        let result = "", readingCell = false;
+        for(let i = 1; i < src.length; i++) {
             if(src.charAt(i) >= 'A' && src.charAt(i) <= 'Z') {
                 result += 'parseFloat(document.querySelectorAll(\'.' + src.charAt(i) + '\')[';
                 readingCell = true;
